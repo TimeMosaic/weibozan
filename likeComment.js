77d206b7f42759569854c8bba94ffbca94324eeb
shanhang.lin <shanhang.lin@thomsonreuters.com>
2017-07-20 19:39:07 +0800

    1. Skip praised comment 2. Auto close poups

--- likeComment.js
+++ likeComment.js
@@ -1,18 +1,28 @@
 
 function aclick(i){
+	if($("a[action-type='ok']").length > 0){
+		$("a[action-type='ok']")[0].click();
+	}
 	a[i].click();
-	if(18 == i){
+	if(k == i){
 		alert( "赞完啦！");
 	}
 }
 a=$("a[action-type='fl_like']");
 
-
 //console.log(a);
 
 cnt = a.length;
-if (cnt >= 19) cnt = 19;
+if (cnt >= 20) cnt = 20;
+var j = 0; // count for praised comments
+var k = 0; //last one index
 for (var i = 0; i < cnt; i++) {
-	var r = Math.random();
-	setTimeout("aclick(" + i + ")", 4000*i +  2000*r);
+	if($(a[i]).children('span').children('span').children('span').hasClass('UI_ani_praised')){
+		j ++;
+		continue;
+	}else{
+		k = i;
+		var r = Math.random();
+		setTimeout("aclick(" + i + ")", 4000*(i-j) +  2000*r);
+	}
 }
