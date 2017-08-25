2ba0cc515242e4aee747b625216ced5043224db6
shanhang.lin <shanhang.lin@thomsonreuters.com>
2017-08-25 19:52:37 +0800

    Some updates.

--- likeComment.js
+++ likeComment.js
@@ -1,5 +1,6 @@
 
 function aclick(i){
+	// close confirm dialog
 	if($("a[action-type='ok']").length > 0){
 		$("a[action-type='ok']")[0].click();
 	}
@@ -11,11 +12,10 @@ function aclick(i){
 a=$("a[action-type='fl_like']");
 
 //console.log(a);
-
 cnt = a.length;
 if (cnt >= 20) cnt = 20;
-var j = 0; // count for praised comments
-var k = 0; //last one index
+j = 0; // count for praised comments
+k = 0; //last one index
 for (var i = 0; i < cnt; i++) {
 	if($(a[i]).children('span').children('span').children('span').hasClass('UI_ani_praised')){
 		j ++;
